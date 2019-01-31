108a6caf2a6976ccba8ba2b8d6794001c1012e11
Ahmed Elkhateeb <ahmedelkhateeb35777@gmail.com>
2019-01-31 10:16:04 +0200

    Configure Database

--- Crowd_Funding/settings.py
+++ Crowd_Funding/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'Crowd_Funding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_pro',
+        'USER': 'root',
+        'PaSSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
-}
+
 
 
 # Password validation
